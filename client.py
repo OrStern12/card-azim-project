266094cc613502709c161bf7a7b4a89de8bd1e98
OrStern12 <orstern1@icloud.com>
2026-01-11 18:18:21 +0200

    reformatted the code and remobed unneeded import

--- client.py
+++ client.py
@@ -7,18 +7,15 @@ client = socket.socket()
 
 def send_data(server_ip, server_port, data):
     client.connect((server_ip, server_port))
-    client.send(((str)(len(data))+' '+data).encode('utf-8'))
+    client.send(((str)(len(data)) + " " + data).encode("utf-8"))
     client.close()
 
 
 def get_args():
-    parser = argparse.ArgumentParser(description='Send data to server.')
-    parser.add_argument('server_ip', type=str,
-                        help='the server\'s ip')
-    parser.add_argument('server_port', type=int,
-                        help='the server\'s port')
-    parser.add_argument('data', type=str,
-                        help='the data')
+    parser = argparse.ArgumentParser(description="Send data to server.")
+    parser.add_argument("server_ip", type=str, help="the server's ip")
+    parser.add_argument("server_port", type=int, help="the server's port")
+    parser.add_argument("data", type=str, help="the data")
     return parser.parse_args()
 
 
@@ -26,11 +23,11 @@ def main():
     args = get_args()
     try:
         send_data(args.server_ip, args.server_port, args.data)
-        print('Done.')
+        print("Done.")
     except Exception as error:
-        print(f'ERROR: {error}')
+        print(f"ERROR: {error}")
         return 1
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     sys.exit(main())
--- server.py
+++ server.py
@@ -1,19 +1,20 @@
 import argparse
-import sys
 import socket
+
 server = socket.socket()
+
 
 def run_server():
     c, address = server.accept()
     print(c.recv(1024).decode())
-    
+
+
 def get_args():
-    parser = argparse.ArgumentParser(description='Send data to server.')
-    parser.add_argument('server_ip', type=str,
-                        help='the server\'s ip')
-    parser.add_argument('server_port', type=int,
-                        help='the server\'s port')
+    parser = argparse.ArgumentParser(description="Send data to server.")
+    parser.add_argument("server_ip", type=str, help="the server's ip")
+    parser.add_argument("server_port", type=int, help="the server's port")
     return parser.parse_args()
+
 
 args = get_args()
 server.bind((args.server_ip, args.server_port))
