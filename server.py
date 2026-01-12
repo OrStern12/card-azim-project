dd3c99d2f82e4308bc51fd70ee5daddb9153c93b
OrStern12 <orstern1@icloud.com>
2026-01-12 18:54:18 +0200

    added documentation and typing

--- server.py
+++ server.py
@@ -4,11 +4,11 @@ import threading
 
 server = socket.socket()
 
-def manage_client(c):
+def manage_client(c: socket.socket):   #function to manage a single client
     print(c.recv(1024).decode())
     c.close()
     
-def get_args():
+def get_args():  # this function deals with the argument received in the beggining
     parser = argparse.ArgumentParser(description="Send data to server.")
     parser.add_argument("server_ip", type=str, help="the server's ip")
     parser.add_argument("server_port", type=int, help="the server's port")
@@ -20,6 +20,6 @@ server.bind((args.server_ip, args.server_port))
 server.listen(10)
 while True:
     c, address = server.accept()
-    thr = threading.Thread(target=manage_client, args=(c,), kwargs={})
+    thr = threading.Thread(target=manage_client, args=(c,), kwargs={}) #thread handeling
     thr.start()
     
