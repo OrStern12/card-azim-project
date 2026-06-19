543289e9a82a57d861b7985877845e6568e79206
OrStern12 <orstern1@icloud.com>
2026-06-19 15:46:26 +0300

    fixed comments

--- driver.py
+++ driver.py
@@ -1,20 +1,26 @@
-from card import *
+from typing import Union
+from os import PathLike
+from card import Card
+from crypt_image import Cryptimage
 import os
 import json
-from card_driver import CardDriver
-from abc import ABC, abstractmethod
+from card_driver import CardDriver, card_id
+
+subdir = r"C:\Users\user"
 
 class Driver(CardDriver):
     def __init__(self):
         self.creator_list = []
+        
     def save(self,  card: Card, dir_path: Union[str, PathLike] = '.'):
         directory_name = f"{dir_path}/{card.creator}"
+        print(directory_name)
         if (not os.path.exists(directory_name)): #checking if directory for creator exists
             os.mkdir(directory_name)
         directory_name += f"/{card.name}"
         os.mkdir(directory_name)
         directory_name+="/metadata.json"
-        image_file_name = f"{self.get_identifier(card)}"+".jpg"
+        image_file_name = f"{self.get_identifier(card).creator}"+f"_{self.get_identifier(card).name}"+".jpg"
         card.cryptimage.image.save(image_file_name)
         json_file = json.dumps({ #creating json
             "name": card.name,
@@ -29,10 +35,10 @@ class Driver(CardDriver):
             file.write(json_file)
 
     def get_identifier(self, card: Card):
-        return (card.name,card.creator)
+        return card_id(card.name,card.creator)
 
-    def load (self, identifier: str):
-        file_name = f"C:/Users/user/{identifier}/metadata.json"
+    def load (self, identifier: card_id):
+        file_name = f"C:/Users/user/{identifier.creator}/{identifier.name}/metadata.json"
         with open(file_name, 'r', encoding='utf-8') as file:
             data = json.load(file)
         cryimage = Cryptimage.create_from_path(data["image_path"]) #creating image from path
@@ -43,9 +49,8 @@ class Driver(CardDriver):
     
     def get_creator_cards(self, creator: str):
         card_list = []
-        directory = f"C:/Users/user/{creator}"
+        directory = os.path.join(subdir,f"{creator}")
         x= next(os.walk(directory)) #going through the directory
-        print(x)
         for name in x[1]:
             file_name = f"{x[0]}/{name}/metadata.json"
             with open(file_name, 'r', encoding='utf-8') as file:
